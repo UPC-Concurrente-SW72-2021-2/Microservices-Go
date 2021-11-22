95ad7832ca4c284e8bc0acfc4ddb5a5f236acac1
Hugo-Ap <araparhug7@gmail.com>
2021-11-22 00:56:57 -0500

    Hotfix svc

--- svc.go
+++ svc.go
@@ -16,8 +16,6 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// cnnnetwork.NeuronalNetworkCNN()
-
 	repository.AddPerson(models.Person{
 		IdPersona:          1,
 		IdVacunadosCovid:   1,
@@ -45,8 +43,6 @@ func main() {
 	router.HandleFunc("/persons", handlers.AddPerson).Methods("POST")
 	router.HandleFunc("/persons", handlers.GetPersons).Methods("GET")
 	router.HandleFunc("/persons/{edad}", handlers.GetPersonsByAge).Methods("GET")
-	// router.HandleFunc("/tasks/{id}", DetelePerson).Methods("DELETE")
-	// router.HandleFunc("/tasks/{id}", UpdatePerson).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
