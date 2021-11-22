db99bb4efd9b887acccd5ea62847857144085584
Hugo-Ap <araparhug7@gmail.com>
2021-11-22 16:31:37 -0500

    update cors

--- svc.go
+++ svc.go
@@ -17,6 +17,25 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome the my GO API!")
 }
 
+func enableCORS(router *mux.Router) {
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	}).Methods(http.MethodOptions)
+	router.Use(middlewareCors)
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, req *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			// and call next handler!
+			next.ServeHTTP(w, req)
+		})
+}
+
 func main() {
 	var personDb []models.PersonJs
 
@@ -43,6 +62,7 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 
+	enableCORS(router)
 	router.HandleFunc("/", indexRoute)
 	router.HandleFunc("/persons", handlers.AddPerson).Methods("POST")
 	router.HandleFunc("/persons", handlers.GetPersons).Methods("GET")
